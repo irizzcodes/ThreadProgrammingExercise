284d599a864493f186be60f78954a504e0deca41
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-10-22 16:48:29 +0800

    add thread on main args

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main extends  Thread {
+    static ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
     public static void main(String[] args) {
+        Main thread = new Main();
+        thread.start();
+        while(thread.isAlive()){
+            System.out.println("Waiting...");
+        }
 
 
     }
@@ -13,7 +21,11 @@ public class Main extends  Thread {
 }
 
 class Storage {
+ private void doStore(int integer){
 
+
+
+ }
 }
 
 class Counter {
