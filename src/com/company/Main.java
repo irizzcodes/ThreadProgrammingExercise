ed4c848a50b76cb78ebaa69552bfeb9c6713083a
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-10-22 17:21:55 +0800

    Final v1 - normal thread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,10 @@ package com.company;
 
 import java.util.ArrayList;
 
+
 public class Main extends  Thread {
-    static ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    Counter counter = new Counter();
+    Printer printer = new Printer();
 
     public static void main(String[] args) {
         Main thread = new Main();
@@ -11,26 +13,44 @@ public class Main extends  Thread {
         while(thread.isAlive()){
             System.out.println("Waiting...");
         }
+        System.out.println("Finish");
 
 
     }
 
     public void run(){
+        counter.doCount();
+        printer.doPrint();
+
 
     }
 }
 
 class Storage {
- private void doStore(int integer){
+     static ArrayList<Integer> list = new ArrayList<Integer>();
 
-
+ static void doStore(int integer){
+     list.add(integer);
 
  }
 }
 
 class Counter {
 
+
+     void doCount(){
+        for (int x = 0; x<11;x++){
+            Storage.doStore(x);
+
+        }
+    }
+
 }
 class Printer {
+     void doPrint(){
+        System.out.println(Storage.list);
+
+    }
+
 
 }
